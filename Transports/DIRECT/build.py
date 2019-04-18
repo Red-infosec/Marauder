3e432479391fd4334555f319f74fc41d7eed9a02
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2019-04-17 20:01:33 -0700

    getting DIRECT working, final fixes maybe

--- Transports/DIRECT/build.py
+++ Transports/DIRECT/build.py
@@ -1,19 +1,26 @@
 import sys
 import json
+import shutil
+from subprocess import call
 
 try:
     with open(sys.argv[1]) as build_file:  
-    build_config = json.load(build_file)
+        build_config = json.load(build_file)
 except Exception as e:
     print("Could not load build config file. Error {}".format(e.Message))
     sys.exit(1)
 
 restore_cmd = "nuget restore ./Transports/DIRECT/packages.config -PackagesDirectory ./Transports/DIRECT/packages"
-
+call(restore_cmd, shell=True)
 
 if build_config["Debug"]:
     configuration = "Debug"
+    output_path = "./Transports/DIRECT/bin/Debug/DIRECT.dll"
 else:
     configuration = "Release"
+    output_path = "./Transports/DIRECT/bin/Release/DIRECT.dll"
 
-build_cmd = "msbuild DIRECT.csproj"
+build_cmd = "msbuild ./Transports/DIRECT/DIRECT.csproj /t:Build /p:Configuration={} /p:TargetFramework={}".format(configuration, build_config["Version"])
+call(build_cmd, shell=True)
+
+shutil.move(output_path, "./output/Transports/DIRECT.dll")
